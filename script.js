d6b5e686a5c6821a92d788772ed672a889f7842f
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2024-10-10 22:07:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+//question 1///
+//   let m= prompt("what is your age?");
+//  if(m>=18){
+//      console.log("you are eligible for vote");
+//  }else{
+// console.log("you are not eligible for vote");
+//  }
+
+
+
+
+// question 2//
+// let a=prompt("ask me a number?")
+// if(a>0){
+//     console.log("number is positive");
+// }else if(a<0){
+//     console.log("number is negative");
+// }else{
+//     console.log("number is zero")
+// }
+
+
+
+
+//question 3//
+//  let user=prompt("Tell me your favourite colour?");
+//  if(user==="red"||user==="blue"||user==="green"||user==="yellow"){
+//     console.log("i like primary colour");
+//  }else{
+// console.log("i don't like primary colour");
+//  }
+
+//question 4//
+// let number=prompt("ask me a number");
+// if(number>=10 && number<=20){
+//     console.log("your number is lie between 10 and 20")
+// }
+
+
+// //question 5//
+//  let marks=prompt("what is your grade?");
+//  if(marks==="a"||marks==="A"|| marks==="b"||marks==="B"|| marks==="c"||marks==="C"|| marks==="d"||marks==="D"){
+//      console.log("you are passed")
+//  }else{
+//  console.log("you are failed");
+//  }
+
+
+
+
+// question 6
+
+// let user=prompt("Enter your age:");
+// let gender=prompt("Enter your gender:");
+
+// if(user>=18 && user<=30 && gender==="male"){
+//     console.log("YOU ARE ELIGIBLE TO SERVE IN MILITARY");
+// }else{
+//     console.log("you are not eligibale to serve in military");
+// }
+
+
+//question 7
+
+// let  m=prompt("enter a number");
+// if(m%2==0){
+//     console.log("its a even number=",m);
+
+// }else{
+//     console.log("its a odd number=",m)
+// }
+
+//question 8
+
+// let a=prompt("Enter a number");
+// if(a%3==0){
+//     console.log(" number is a multiple of 3");
+// }
+
+
+
